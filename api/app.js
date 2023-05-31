1897f741a716dc17204725e56df95f13d056936c
popoxta <popoxta@gmail.com>
2023-06-01 11:36:22 +1200

    passportjs and session setup

--- api/app.js
+++ api/app.js
@@ -1,5 +1,9 @@
 const express = require('express')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
 const connection = require('./config/database')
+require('dotenv').config()
 
 const app = express()
 
@@ -11,6 +15,25 @@ app.use((req, res, next) => {
    console.log(`New request at ${req.path} at ${new Date()}`)
     next()
 })
+
+// SESSION //
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_CONNECT
+    }),
+    cookie: {
+        maxAge: 86400000,
+    }
+}))
+
+// PASSPORT //
+require('./config/passport')
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use((req, res) => res.send('I\'ll end it here.'))
 

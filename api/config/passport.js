e45816ff5463a88c6d0d909f263bb1be5640f325
popoxta <popoxta@gmail.com>
2023-06-01 11:29:15 +1200

    add passportjs local strategy

--- api/config/passport.js
+++ api/config/passport.js
@@ -1,8 +1,33 @@
 const passport = require('passport')
+const User = require('../models/user')
 const LocalStrategy = require('passport-local').Strategy
 
-passport.use(new LocalStrategy({
-    async function(username, password, cb) {
+passport.use(new LocalStrategy(
+    async (username, password, done) => {
+        try {
+            // check if user with username exists
+            const user = await User.findOne({username})
+            if (!user) return done(null, false, {message: 'User does not exist.'})
 
+            // check if password entered correctly
+            bcrypt.compare(password, user.hash, (error, valid) => {
+                if (valid) return done(null, user)
+                else return done(null, false, {message: 'Incorrect password.'})
+            })
+        } catch (err) {
+            return done(err)
+        }
     }
-}))
+))
+
+passport.serializeUser((user, cb) => cb(null, user.id))
+
+passport.deserializeUser(async (userID, done) => {
+    try {
+        const user = await User.findById(userID)
+        done(null, user)
+
+    } catch (err) {
+        done(err)
+    }
+})
--- api/models/user.js
+++ api/models/user.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose').default
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true},
     hash: {type: String, required: true},
-    salt: {type: String, required: true},
 })
 
 const User = mongoose.model('User', UserSchema)

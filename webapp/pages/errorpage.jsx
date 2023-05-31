5da8729bb7948f9f3f4a0643a368997742e7dba6
popoxta <popoxta@gmail.com>
2023-06-01 09:45:40 +1200

    add error route

---
+++ webapp/pages/errorpage.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/header.jsx";
+
+export default function ErrorPage() {
+    return (
+        <>
+            <Header/>
+            <h1>OOPS! An error has occurred...</h1>
+            <h2>Please try again.</h2>
+        </>
+    )
+}
--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,9 +1,10 @@
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
 import MainLayout from "../pages/components/main.jsx";
 import Home from "../pages/home.jsx";
+import ErrorPage from "../pages/errorpage.jsx";
 
 const router = createBrowserRouter(createRoutesFromElements(
-    <Route path={'/'} element={<MainLayout/>} id={'root'}>
+    <Route path={'/'} element={<MainLayout/>} id={'root'} errorElement={<ErrorPage/>}>
         <Route index element={<Home/>}/>
     </Route>
 ))

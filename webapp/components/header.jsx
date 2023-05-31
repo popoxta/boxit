79d417a120857ea15cf9ff5024855fa352403759
popoxta <popoxta@gmail.com>
2023-06-01 09:38:44 +1200

    add header component and basic css style

---
+++ webapp/components/header.jsx
@@ -0,0 +1,15 @@
+import {Link} from "react-router-dom";
+
+export default function Header() {
+    return (
+        <div className={'header shadow flex apart'}>
+            <Link to={'/'}><h1>BOXIT</h1></Link>
+
+            <div className={'flex'}>
+                <Link to={'/boxes'}>boxes</Link>
+                <Link to={'/login'}>login</Link>
+            </div>
+
+        </div>
+    )
+}
--- webapp/components/main.jsx
+++ webapp/components/main.jsx
@@ -1,9 +1,11 @@
 import {Outlet} from "react-router-dom";
+import Header from "./header.jsx";
 
-export default function MainLayout(){
+export default function MainLayout() {
     return (
         <>
-        <h1>Main Layout</h1>
+            <Header/>
+            <h1>Main Layout</h1>
             <Outlet/>
         </>
     )

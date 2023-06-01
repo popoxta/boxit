c63f53c4ade78abf243a0aa7be7b3f7b0a56de4b
popoxta <popoxta@gmail.com>
2023-06-01 12:13:04 +1200

    update errorpage to inc. error msg

--- webapp/pages/errorpage.jsx
+++ webapp/pages/errorpage.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/header.jsx";
+import {useRouteError} from "react-router-dom";
 
 export default function ErrorPage() {
+    const error = useRouteError()
     return (
         <>
             <Header/>
             <h2>OOPS! An error has occurred...</h2>
-            <h3>Please try again.</h3>
+            <h3>{error.message}</h3>
         </>
     )
 }

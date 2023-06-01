abe11e764a534ba4819286cec4e4a9d260588179
popoxta <popoxta@gmail.com>
2023-06-02 11:22:46 +1200

    refactor jsx file names to pascal case

--- webapp/pages/components/Main.jsx
+++ webapp/pages/components/Main.jsx
@@ -1,5 +1,5 @@
 import {Outlet} from "react-router-dom";
-import Header from "./header.jsx";
+import Header from "./Header.jsx";
 
 export default function MainLayout() {
     return (
--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,17 +1,16 @@
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
-import MainLayout from "../pages/components/main.jsx";
-import Home from "../pages/home.jsx";
-import ErrorPage from "../pages/errorpage.jsx";
-import Register, {action as registerAction} from "../pages/register.jsx";
-import Login, {action as loginAction} from "../pages/login.jsx";
-import Boxes, {loader as boxesLoader} from "../pages/boxes.jsx";
+import MainLayout from "../pages/components/Main.jsx";
+import Home from "../pages/Home.jsx";
+import ErrorPage from "../pages/ErrorPage.jsx";
+import Register, {action as registerAction} from "../pages/Register.jsx";
+import Login, {action as loginAction} from "../pages/Login.jsx";
+import Boxes, {loader as boxesLoader} from "../pages/Boxes.jsx";
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path={'/'} element={<MainLayout/>} id={'root'} errorElement={<ErrorPage/>}>
         <Route index element={<Home/>} />
         <Route path={'/register'} element={<Register/>} action={registerAction}/>
         <Route path={'/login'} element={<Login/>} action={loginAction}/>
-
         <Route path={'/boxes'} element={<Boxes/>} loader={boxesLoader}/>
     </Route>
 ))

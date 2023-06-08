4920e49d507430a540e633af988400cc5b9c1b33
popoxta <popoxta@gmail.com>
2023-06-09 08:53:58 +1200

    refactor deletion method to return res to handler

--- webapp/pages/items/DeleteItem.jsx
+++ webapp/pages/items/DeleteItem.jsx
@@ -1,4 +1,4 @@
-import {Link, redirect, useLoaderData, useSearchParams} from "react-router-dom";
+import {Link, redirect, useLoaderData, useNavigate, useSearchParams} from "react-router-dom";
 export async function loader({params}) {
     const itemId = params.id
     const res = await fetch(`http://localhost:3000/items/${itemId}`, {
@@ -7,28 +7,34 @@ export async function loader({params}) {
     return await res.json()
 }
 
-async function deleteItem(item, redirectLocation){
+async function deleteItem(item){
     const itemId = item._id
 
     const res = await fetch(`http://localhost:3000/items/${itemId}/delete`, {
         method: 'DELETE',
         credentials: 'include',
-        body: JSON.stringify({
-            id: itemId
-        })
     })
     
     const result = await res.json()
-    if (res.status === 200) return redirect(redirectLocation)
+    if (res.status === 200) return res
     else return result
 }
 
 export default function DeleteItem() {
     const loaderData = useLoaderData()
     const [location] = useSearchParams()
+    const navigate = useNavigate()
+
     const prevLocation = location.get('from') ?? '/items'
-    const errors = loaderData.message
+
+    let errors = loaderData.message
     const item = loaderData.item
+
+    async function handleDelete() {
+        const deletion = await deleteItem(item)
+        if (deletion.status === 200) return navigate(prevLocation)
+        else errors = deletion.message
+    }
 
     return (
         <div className={'flex column'}>
@@ -39,12 +45,12 @@ export default function DeleteItem() {
             {errors
                 ? <>
                     <h2>Error</h2>
-                    <h3>{errors.message}</h3>
+                    <h3>{errors}</h3>
                 </>
                 : <>
                     <h2>{item.name}</h2>
                     <h3>Are you sure you want to delete {item.name}?</h3>
-                    <button onClick={()=>deleteItem(item, prevLocation)}>Delete</button>
+                    <button onClick={handleDelete}>Delete</button>
                 </>
             }
         </div>
